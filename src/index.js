b67c656bbc22b6f55313aaceeae6872924a6c8eb
Hashim <hsqworld@hotmail.com>
2022-11-16 03:55:36 +0500

    A separte component with header added.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
-function FirtComponent(){
+function Header(){
   return(
-    <header>
-      <nav>
-        <img src="./logo192.png" alt='Simply easy' width=""/>  
-      </nav>
+  <header>
+        <nav>
+          <img src="./logo192.png" alt='Simply easy' width="40px" />
+        </nav>
+      </header>
+    )
+}
+
+function FirtComponent() {
+  return (
+    <div>
+      <Header />
       <h1>Reasons I am excited to learn React</h1>
       <ol>
-      <li>It's a popular library, so ill be 
-        able to fit in with the cool kids.
-      </li>
-      <li>I will change my life with react</li>
-      <li>I'm more likely to get a job as as developer
-        if I know react.
-      </li>
+        <li>It's a popular library, so ill be
+          able to fit in with the cool kids.
+        </li>
+        <li>I will change my life with react</li>
+        <li>I'm more likely to get a job as as developer
+          if I know react.
+        </li>
       </ol>
-      <footer>@ 20xx Shafique Development. All rights reserved. </footer>
-      </header>
-    
+      <footer>
+        <small> ©2022 Shafique Development. All rights reserved. </small>
+      </footer>
+    </div>
+
   )
 }
 
