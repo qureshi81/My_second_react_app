217b7e5c5f3ff1ca70245560ce93d0d597a6eb63
Hashim <hsqworld@hotmail.com>
2022-11-25 04:20:18 +0500

    Introduced how to move component in separate file and reference them

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+
+
+
+export function Header(){
+    return(
+    <header>
+          <nav className='nav'>
+            <img src="./logo192.png" alt='Simply easy' className="nav-logo"/>
+            <ul className='nav-items'>
+              <li>Pricing</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        </header>
+      )
+  }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import "./style.css"
+import { Header } from './Header';
 
 
-function Header(){
-  return(
-  <header>
-        <nav className='nav'>
-          <img src="./logo192.png" alt='Simply easy' className="nav-logo"/>
-          <ul className='nav-items'>
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-      </header>
-    )
-}
+
 
 function FirtComponent() {
   return (
